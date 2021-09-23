c47c39029f45bc02a065daaf7d1cfa945ed2e5a5
Allison Falls <allison.falls2@gmail.com>
2021-09-23 11:16:19 -0400

    finished

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,24 +1,37 @@
 package com.codeup.springblog.controllers;
 
 
+import com.codeup.springblog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @Controller
 
 public class PostController {
     @GetMapping( "/posts")
-    @ResponseBody
-    public String posts(){
-        return "posts index page";
+    public String showPosts(Model model){
+
+        List<Post> allPosts = new ArrayList<>();
+
+        allPosts.add(new Post("post!", "post1 body"));
+        allPosts.add(new Post("post@", "post2 body"));
+
+        model.addAttribute("posts", allPosts);
+        return "post/index";
     }
 
     @GetMapping( "/posts/{id}")
-    @ResponseBody
-    public String showSinglePost(@PathVariable int id){
-        return "view an individual post" + id;
+    public String showSinglePost(@PathVariable int id, Model model){
+
+        Post post = new Post("Fun title", "fun body");
+        model.addAttribute("postId", id);
+        model.addAttribute("post", post);
+        return "post/show";
     }
 
     @GetMapping("/posts/create")
